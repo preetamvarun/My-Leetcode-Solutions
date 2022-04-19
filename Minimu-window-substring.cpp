fca418ca979104b90622285d609b90897c158205
preetam varun <49122112+preetamvarun@users.noreply.github.com>
2022-04-19 12:48:32 +0530

    Minimu-window-substring

--- Minimu-window-substring.cpp
+++ Minimu-window-substring.cpp
@@ -1,21 +1,56 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int i = 0, j = 0, len = s.size(), count = 0, ans = 0;
+    string minWindow(string s, string t) {
+        if(t.size() > s.size()) return "";
         map<char,int>mp;
+        int i = 0, j = 0, count = 0, len = s.size(), ans = INT_MAX;
+        int start_index = -1, end_index = -1;
+        string ans_string = "";
+        for(int i = 0; i < t.size(); i++) mp[t[i]]++;
+        count = mp.size();
         while(j < len){
-            mp[s[j]]++;
-            count++;
-            if(mp[s[j]] == 1){
-                ans = max(ans,count);
-            }
-            while(mp[s[j]] > 1){
-                mp[s[i]]--;
-                i++;
-                count--;
+            if(mp.find(s[j]) != mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]] == 0) count--;
+                if(count == 0){
+                    if(j-i+1 < ans){
+                        start_index = i;
+                        end_index = j;
+                        ans = end_index - start_index + 1;
+                    }
+                }
+                while(count == 0){
+                    if(mp.find(s[i]) != mp.end()){
+                        mp[s[i]]++;
+                        if(mp[s[i]] == 1){
+                            count++;
+                        }
+                        else if(mp[s[i]] <= 0){
+                            if(j - (i+1) + 1 < ans){
+                                start_index = i+1;
+                                end_index = j;
+                                ans = end_index - start_index + 1;
+                            }
+                        }
+                    }                    
+                    else{
+                        if(j - (i+1) + 1 < ans){
+                            start_index = i + 1;
+                            end_index = j;
+                            ans = end_index - start_index + 1;
+                        }
+                    }
+                    i++;
+                }
             }
             j++;
         }
-        return ans;
+        if(start_index == -1 || end_index == -1){
+            return "";
+        }
+        for(int i = start_index; i <= end_index; i++){
+            ans_string += s[i];
+        }
+        return ans_string;
     }
 };
