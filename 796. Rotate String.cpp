1ddc067a61a8605784bce4cf2bbc1c7dbf669a38
preetam varun <49122112+preetamvarun@users.noreply.github.com>
2022-04-22 19:41:41 +0530

    796. Rotate String.cpp

--- 796. Rotate String.cpp
+++ 796. Rotate String.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        string rs = s;
-        rs.push_back(rs[0]);
-        rs.erase(rs.begin());
-        while(rs != s){
-            rs.push_back(rs[0]);
-            rs.erase(rs.begin());
-            if(rs == goal){
+        string rotate_str = s;
+        rotate_str.push_back(rotate_str[0]);
+        rotate_str.erase(rotate_str.begin());
+        while(rotate_str != s){
+            rotate_str.push_back(rotate_str[0]);
+            rotate_str.erase(rotate_str.begin());
+            if(rotate_str == goal){
                 return true;
             }
         }
